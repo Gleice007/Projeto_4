a2eebadd7efcbcf818d65e9c15bea9d1ff80fca4
Gleiciele <gleicielerodrigues57@gmail.com>
2024-06-28 16:37:35 -0300

    projeto-4

---
+++ projeto04.py
@@ -0,0 +1,11 @@
+"""
+Escreva um programa que peça o primeiro nome
+e o sobrenome do usuário e exiba-os em 
+diferentes formatos.
+"""
+
+nome = input('Digite seu primeiro nome: ')
+sobrenome = input("Agora digite seu sobrenome: ")
+
+print(nome) 
+print(sobrenome)
